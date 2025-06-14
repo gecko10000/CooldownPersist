e208cfc8228d56afd15ed22143bb956819ab4f84
gecko10000 <levtey@gmail.com>
2025-06-13 21:49:45 -0700

    Take GC code out of listeners

--- src/gecko10000/cooldownpersist/CooldownPersist.java
+++ src/gecko10000/cooldownpersist/CooldownPersist.java
@@ -1,11 +1,45 @@
 package gecko10000.cooldownpersist;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+
 public class CooldownPersist extends JavaPlugin {
+
+    private static final long GC_INTERVAL = 20 * 60 * 10; // 10 minutes
+
+    public final Map<UUID, Map<NamespacedKey, Integer>> allCooldowns = new HashMap<>();
 
     @Override
     public void onEnable() {
         new Listeners(this);
+        this.getServer().getScheduler().runTaskTimer(this, this::gc, GC_INTERVAL, GC_INTERVAL);
     }
+
+    private void gc() {
+        int currentTick = this.getServer().getCurrentTick();
+        Iterator<UUID> uuidIterator = allCooldowns.keySet().iterator();
+        while (uuidIterator.hasNext()) {
+            UUID uuid = uuidIterator.next();
+            // Remove individual expired cooldowns
+            Map<NamespacedKey, Integer> cooldowns = allCooldowns.get(uuid);
+            Iterator<Map.Entry<NamespacedKey, Integer>> cooldownIterator = cooldowns.entrySet().iterator();
+            while (cooldownIterator.hasNext()) {
+                Map.Entry<NamespacedKey, Integer> cooldown = cooldownIterator.next();
+                if (currentTick <= cooldown.getValue()) continue;
+                //plugin.getLogger().info("Removing " + cooldown.getKey());
+                cooldownIterator.remove();
+            }
+            // Remove empty cooldown maps
+            if (cooldowns.isEmpty()) {
+                //plugin.getLogger().info("Removing " + uuid);
+                uuidIterator.remove();
+            }
+        }
+    }
+
 }
--- src/gecko10000/cooldownpersist/Listeners.java
+++ src/gecko10000/cooldownpersist/Listeners.java
@@ -9,35 +9,29 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.UUID;
 
 public class Listeners implements Listener {
 
-    private static final long GC_INTERVAL = 20 * 60 * 10; // 10 minutes
-
     private final CooldownPersist plugin;
-    // Stores end ticks of the cooldowns.
-    private final Map<UUID, Map<NamespacedKey, Integer>> allCooldowns = new HashMap<>();
 
     public Listeners(CooldownPersist plugin) {
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        plugin.getServer().getScheduler().runTaskTimer(plugin, this::gc, GC_INTERVAL, GC_INTERVAL);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     private void onUseItem(PlayerItemGroupCooldownEvent event) {
         UUID uuid = event.getPlayer().getUniqueId();
         int endTick = plugin.getServer().getCurrentTick() + event.getCooldown();
-        allCooldowns.computeIfAbsent(uuid, u -> new HashMap<>()).put(event.getCooldownGroup(), endTick);
+        plugin.allCooldowns.computeIfAbsent(uuid, u -> new HashMap<>()).put(event.getCooldownGroup(), endTick);
     }
 
     @EventHandler
     private void onAdd(EntityAddToWorldEvent event) {
         if (!(event.getEntity() instanceof HumanEntity player)) return;
-        Map<NamespacedKey, Integer> cooldowns = allCooldowns.get(player.getUniqueId());
+        Map<NamespacedKey, Integer> cooldowns = plugin.allCooldowns.get(player.getUniqueId());
         if (cooldowns == null) return;
         int currentTick = plugin.getServer().getCurrentTick();
         cooldowns.forEach((key, endTick) -> {
@@ -45,28 +39,6 @@ public class Listeners implements Listener {
             if (cooldown < 1) return;
             player.setCooldown(key, cooldown);
         });
-    }
-
-    private void gc() {
-        int currentTick = plugin.getServer().getCurrentTick();
-        Iterator<UUID> uuidIterator = allCooldowns.keySet().iterator();
-        while (uuidIterator.hasNext()) {
-            UUID uuid = uuidIterator.next();
-            // Remove individual expired cooldowns
-            Map<NamespacedKey, Integer> cooldowns = allCooldowns.get(uuid);
-            Iterator<Map.Entry<NamespacedKey, Integer>> cooldownIterator = cooldowns.entrySet().iterator();
-            while (cooldownIterator.hasNext()) {
-                Map.Entry<NamespacedKey, Integer> cooldown = cooldownIterator.next();
-                if (currentTick <= cooldown.getValue()) continue;
-                //plugin.getLogger().info("Removing " + cooldown.getKey());
-                cooldownIterator.remove();
-            }
-            // Remove empty cooldown maps
-            if (cooldowns.isEmpty()) {
-                //plugin.getLogger().info("Removing " + uuid);
-                uuidIterator.remove();
-            }
-        }
     }
 
 }
